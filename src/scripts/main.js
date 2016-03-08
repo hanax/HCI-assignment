c1abfad5afb562d11598e7eb5e35644305261b58
hanax <alfalfa.xue@gmail.com>
2016-03-07 20:00:27 -0500

    style fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,29 @@ import 'normalize-css';
 import '../styles/index.styl';
 
 $(() => {
+  const windowHeight = $(window).height();
+  const windowWidth = $(window).width();
+
+  $(window).scroll(() => {
+    if ($("body").scrollTop() > windowHeight / 4) {
+      $(".header-stick .title").fadeIn("slow");
+    } else {
+      $(".menu-dropdown").fadeOut("fast");
+      $(".header-stick .title").fadeOut("slow");
+    }
+  });
+
+  $(".menu").click(() => {
+    $(".menu-dropdown").fadeToggle("fast");
+  });
+
+  $("body").click((e) => {
+    const t = $(e.target);
+    if (!t.closest(".menu").length && !t.closest(".menu-dropdown").length) {
+      $(".menu-dropdown").fadeOut("fast");
+    }
+  });
+
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
